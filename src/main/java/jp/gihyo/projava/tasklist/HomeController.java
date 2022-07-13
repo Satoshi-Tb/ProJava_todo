c87cdd724dbb2f4d2cb275d4109942ab30105af0
Satoshi-Tb <sulp91_fy@yahoo.co.jp>
2022-07-14 01:04:05 +0900

    タスク表示、登録画面作成

--- src/main/java/jp/gihyo/projava/tasklist/HomeController.java
+++ src/main/java/jp/gihyo/projava/tasklist/HomeController.java
@@ -1,15 +1,22 @@
 package jp.gihyo.projava.tasklist;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
-
+	record TaskItem(String id, String task, String deadline, boolean done) {}
+	private List<TaskItem> taskList = new ArrayList<TaskItem>();
+	
 	@RequestMapping(value = "/hellohtml")
 	@ResponseBody
 	String hello() {
@@ -32,4 +39,18 @@ public class HomeController {
 		return "hello";  //対応するビュー名を返す。
 	}
 	
+	@GetMapping("/list")
+	String  list(Model model) {
+		model.addAttribute("taskList", taskList);
+		return "home";
+	}
+	
+	@GetMapping("/add")
+	String addTask(@RequestParam("task")  String task,  @RequestParam("deadline") String deadline) {
+		String id = UUID.randomUUID().toString().substring(0, 8);
+		TaskItem item = new TaskItem(id, task, deadline, false);
+		taskList.add(item);
+		
+		return "redirect:/list";
+	}
 }
